0f54987dfe540b5abc57bf5c1af9c1b1a58b9db2
Maunas <92191116+Maunas@users.noreply.github.com>
2024-11-15 01:09:45 +0000

    Transformada imagen antes de predecir

--- prod/app.py
+++ prod/app.py
@@ -1,12 +1,14 @@
 import torch
 import streamlit as st
 from utils import load_model, predict_and_plot_image 
+from PIL import Image
 
 model, device = load_model()
-image = st.camera_input("Sube una foto")
+image = st.file_uploader("Sube una foto", type=["png","jpg"])
 # Check if an image is uploaded before attempting to process it
 if image is not None:
-    plot = predict_and_plot_image(model, image, device)
+    imagePIL = Image.open(image)
+    plot = predict_and_plot_image(model, imagePIL, device)
     st.pyplot(plot)
 else:
     st.write("Sube una foto.")
