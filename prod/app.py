38500e9a6810b7e097e3959372b38a00627e1478
Maunas <92191116+Maunas@users.noreply.github.com>
2024-11-15 01:31:55 +0000

    Cambiado a input camara

--- prod/app.py
+++ prod/app.py
@@ -3,7 +3,7 @@ import streamlit as st
 from utils import load_model, predict_and_plot_image 
 
 model, device = load_model()
-image = st.file_uploader("Sube una foto", type=["png","jpg"])
+image = st.camera_input("Sube una foto", type=["png","jpg"])
 # Check if an image is uploaded before attempting to process it
 if image is not None:
     plot = predict_and_plot_image(model, image, device)
