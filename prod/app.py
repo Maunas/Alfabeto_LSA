0480594189cc07690c03117fb43e8f3b4f017026
Maunas <92191116+Maunas@users.noreply.github.com>
2024-11-15 00:38:39 +0000

    Creada funcion para predecir una imagen

--- prod/app.py
+++ prod/app.py
@@ -1,4 +1,8 @@
 import torch
 import streamlit as st
+import load_model, predict_and_plot_image, label_map from utils
 
-st.file_uploader("Sube una foto", type=["png","jpg"])
+model, device = load_model()
+image = st.file_uploader("Sube una foto", type=["png","jpg"])
+plot = predict_and_plot_image(model, image, device, label_map)
+st.pyplot(plot)
--- prod/utils.py
+++ prod/utils.py
@@ -23,3 +23,38 @@ def load_model():
     model.load_state_dict(torch.load('./modelo.pt',weights_only=True, map_location=device))
     model.eval()
     return model, device
+
+def predict_and_plot_image(model, image, device, label_map):
+  # Convertir la imagen al formato de numpy y moverla a CPU
+  imagePIL = T.ToPILImage()(image.cpu())
+  imagePIL = T.ToTensor()(imagePIL).permute(1, 2, 0).numpy()
+
+  # Ejecuta el modelo en la imagen y obtén las predicciones
+  model.eval()
+  with torch.no_grad():
+      detections = model([image.to(device)])[0]
+
+  boxes = [detections['boxes'][0]]
+  labels = [detections['labels'][0]]
+  scores = [detections['scores'][0]]
+
+  # Graficar la imagen y las cajas
+  plt.figure(figsize=(12, 8))
+  plt.imshow(imagePIL)
+  for i, box in enumerate(boxes):
+      xmin, ymin, xmax, ymax = box.cpu()
+      plt.gca().add_patch(plt.Rectangle((xmin, ymin), xmax - xmin, ymax - ymin,
+                                        edgecolor='green', fill=False, linewidth=2))
+      # Etiqueta con la clase y el puntaje de confianza
+      label = f"Class: {get_letter_from_number(labels[i].item())}, Score: {scores[i].item():.2f}"
+      plt.text(xmin, ymin, label, color='white', fontsize=10,
+              bbox=dict(facecolor='green', edgecolor='green', alpha=0.5))
+  plt.axis('off')
+  return plt
+
+label_map = {
+    "A": 1, "B": 2, "C": 3, "D": 4, "E": 5, "F": 6, "G": 7, "H": 8, "I": 9,
+    "J": 10, "K": 11, "L": 12, "M": 13, "N": 14, "O": 15, "P": 16, "Q": 17,
+    "R": 18, "S": 19, "T": 20, "U": 21, "V": 22, "W": 23, "X": 24, "Y": 25,
+    "Z": 26
+}
