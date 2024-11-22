62d3892f6c96037b9c61bb6642351ded5ebc5047
Maunas <92191116+Maunas@users.noreply.github.com>
2024-11-22 00:47:02 +0000

    Ambas opciones funcionan a la vez

--- prod/app.py
+++ prod/app.py
@@ -3,12 +3,20 @@ import streamlit as st
 from utils import load_model, predict_and_plot_image 
 
 model, device = load_model()
+gallery = []
+
 st.title("Detector de alfabeto LSA")
-image = st.camera_input("Saca una foto de la seña")
-image = st.file_uploader("Sube una foto", type=["png", "jpg"])
+st.write("Sacate una foto realizando una seña del alfabeto, o sube una foto al sistema.")
+image_camera = st.camera_input("Saca una foto de la seña")
+image_file = st.file_uploader("Sube una foto", type=["png", "jpg"])
+
 # Check if an image is uploaded before attempting to process it
-if image is not None:
-    plot = predict_and_plot_image(model, image, device)
+if image_camera is not None :
+    plot = predict_and_plot_image(model, image_camera, device)
     st.pyplot(plot)
-else:
-    st.write("Sube una foto.")
+    gallery.append(plot)
+
+if image_file is not None :
+    plot = predict_and_plot_image(model, image_file, device)
+    st.pyplot(plot)
+    gallery.append(plot)
